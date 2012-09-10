7c8d2fd9d3cf33e0e2af5a78accdedaa12f5d251
Nesk <pardanaud.j+mailforrobots@gmail.com>
2012-09-10 23:13:43 +0200

    Removed unnecessary IEF

--- contentscript.js
+++ contentscript.js
@@ -1,55 +1,50 @@
+// This script is necessary to browse the HTML content and find the background URL
+
+
 /*
- * This script is necessary to browse the HTML content and find the background URL
+ * Properties
  */
 
-(function() {
-
-    /*
-     * Properties
-     */
-
-    var port = chrome.extension.connect(),
-        target; // Contains the latest clicked element, needed because Chrome contextMenus can't provide it
+var port = chrome.extension.connect(),
+    target; // Contains the latest clicked element, needed because Chrome contextMenus can't provide it
 
 
-    /*
-     * Functions
-     */
+/*
+ * Functions
+ */
 
-    function getBackgroundUrl() {
-        var el = target,
-            style,
-            url = 'none';
+function getBackgroundUrl() {
+    var el = target,
+        style,
+        url = 'none';
 
-        if(!el) {
-            port.postMessage('error'); // Unexpected error, the user must be alerted
-        }
-
-        do {
-            style = getComputedStyle(el);
-            style && (url = style.backgroundImage);
-        } while((el = el.parentNode) && url == 'none');
-
-        if(~url.indexOf('gradient(') || url == 'none') { // Avoids CSS3 gradients and none values
-            url = null; // Sets url to none, it will alert the user that there's no background image
-        } else {
-            url = url.slice(4, -1); // "backgroundImage" property also provides the "url()" characters, we need to remove them
-        }
-
-        return url;
+    if(!el) {
+        port.postMessage('error'); // Unexpected error, the user must be alerted
     }
 
+    do {
+        style = getComputedStyle(el);
+        style && (url = style.backgroundImage);
+    } while((el = el.parentNode) && url == 'none');
 
-    /*
-     * Events initialization
-     */
+    if(~url.indexOf('gradient(') || url == 'none') { // Avoids CSS3 gradients and none values
+        url = null; // Sets url to none, it will alert the user that there's no background image
+    } else {
+        url = url.slice(4, -1); // "backgroundImage" property also provides the "url()" characters, we need to remove them
+    }
 
-    document.body.addEventListener('mousedown', function(e) {
-        target = e.target;
-    }, false);
+    return url;
+}
 
-    port.onMessage.addListener(function() {
-        port.postMessage(getBackgroundUrl()); // Transmits the background URL to the background script, even if there's no one
-    });
 
-})();
+/*
+ * Events initialization
+ */
+
+document.body.addEventListener('mousedown', function(e) {
+    target = e.target;
+}, false);
+
+port.onMessage.addListener(function() {
+    port.postMessage(getBackgroundUrl()); // Transmits the background URL to the background script, even if there's no one
+});
